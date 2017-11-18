0c24bfdc4f3a22a287a28d9b54537de5b3d2046b
Andrew Lipsey <adlipsey@gmail.com>
2017-11-18 12:57:33 -0500

    Added get route for bg images

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,6 +11,10 @@ module.exports = function(app){
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
 
+	app.get("/assets/images/ignasi.png", function(req, res){
+		res.sendFile(path.join(__dirname, "../assets/images/ignasi.png"));
+	});
+
 	app.get("*", function(req, res){
 		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
